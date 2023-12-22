7f481d50a577f421c5d04985eab0737a5fc968dd
José Otegui Marín <115463004+JoseOtegui@users.noreply.github.com>
2023-12-22 01:41:29 +0100

    Add files via upload

---
+++ practica2/id3.js
@@ -0,0 +1,157 @@
+// Para probar la carga y lectura de archivos
+var ATRIBUTOS = [];
+var DATASET = [];
+var DATASETAUX = [];
+var ATRIBUTOSAUX = [];
+
+var IT = 1;
+
+$(function(){
+    $('.load').click(function(){
+        var attrs = $('input[name="attrs"]').get(0).files;
+        var examples = $('input[name="content"]').get(0).files;
+
+        if(attrs.length > 0 && examples.length > 0){
+            attrs = attrs[0];
+            examples = examples[0];
+
+            $('.info .file.attrs-file').html(attrs.name);
+            $('.info .file.dataset-file').html(examples.name);
+
+            $.each([ attrs, examples ], function(i, file){
+                var reader = new FileReader();
+                reader.readAsText(file, "UTF-8");
+                reader.onload = function(event){
+                    if(i == 0){
+                        ATRIBUTOS = event.target.result.split(',');
+                        ATRIBUTOSAUX = event.target.result.split(',');
+                        console.log('$ATRIBUTOS', ATRIBUTOS);
+                    } else{
+                        DATASET = event.target.result.split('\n');
+                        DATASETAUX = event.target.result.split('\n');
+                        console.log('$DATASET', DATASET);
+                    }
+                };
+            });
+
+            $('button.execute').prop('disabled', false);
+        } else{
+            alert('IMPORTANTE. Es obligatorio cargar los dos archivos.');
+        }
+    });
+
+    $('.execute').click(function(){
+        console.warn('--------------------------');
+        recursivo(ATRIBUTOS, DATASET, IT);
+    });
+});
+
+//////////////////////////////////////////////////
+
+function recursivo(atributos, dataset, it){
+    var datos = algoritmo(atributos, dataset);
+    
+    // sacamos el MENOR merito
+    var level = {}, _merito = 99999999999, _key = "";
+    $.each(datos, function(attrKey, attr){
+        if(attr.merito < _merito){
+            _key = attrKey;
+            _merito = attr.merito;
+            level = attr;
+            console.debug('NIVEL', level);
+            delete(level.merito);
+        }
+    });
+
+    $('.tree').append(`<li class="nodo" style="margin-left: ${it * 10}px;">${_key}</li>`);
+
+    $.each(level, function(rama, valor){
+        $('.tree').append(`<li class="rama" style="margin-left: ${it * 20}px;">${rama}</li>`);
+
+        DATASETAUX = filtrar(rama, DATASETAUX);
+        console.debug('ESTAMOS EN LA RAMA', rama);
+
+        if(valor.positivo === 0 || valor.negativo === 0){
+            console.warn('FINALIZADO', (valor.negativo === 0 ? "si" : "no") + " se puede salir a jugar");
+        } else{
+            var _attrs = ATRIBUTOSAUX;
+            _attrs.splice(_attrs.indexOf(_key), 1);
+            ATRIBUTOSAUX = _attrs;
+            recursivo(_attrs, DATASETAUX, it + 1);
+        }
+
+        // volver a cargar la variable DATASET
+        DATASETAUX = DATASET;
+        ATRIBUTOSAUX = ATRIBUTOS;
+    });
+}
+
+function filtrar(nombreRama, datos){
+    var _filter = [];
+
+    $.each(datos, function(i, item){
+        var idx = item.split(',').indexOf(nombreRama);
+        if(idx >= 0){
+            var aux = item.split(',');
+            aux.splice(idx, 1);
+            _filter.push(aux.join(','));
+        }
+    });
+
+    return _filter;
+}
+
+function algoritmo(atributos, dataset){
+    var datos = {};
+
+    // preparamos el objeto poniendo en las claves cada uno de los atributos
+    for(var i = 0; i < atributos.length - 1; i++){
+        datos[atributos[i]] = {};
+    }
+
+    $.each(dataset, function(i, fila){
+        fila = fila.replace('\r', '').split(',');
+        var sol = fila[fila.length - 1]; // si o no
+
+        for(var i = fila.length - 2; i >= 0; i--){
+            if(!datos[atributos[i]][fila[i]]){
+                datos[atributos[i]][fila[i]] = {
+                    positivo    : sol === "si" ? 1 : 0,
+                    negativo    : sol === "no" ? 1 : 0,
+                    total       : 1
+                };
+            } else{
+                if(sol === "si"){
+                    datos[atributos[i]][fila[i]].positivo++;
+                } else{
+                    datos[atributos[i]][fila[i]].negativo++;
+                }
+                datos[atributos[i]][fila[i]].total++;
+            }
+        }
+    });
+
+    // Calculamos el merito
+    for(var i = 0; i < atributos.length - 1; i++){
+        merito(datos, atributos[i], dataset.length);
+    }
+
+    return datos;
+}
+
+function merito(data, key, N){
+    var merito = 0;
+
+    $.each(data[key], function(k, v){
+        var r = v.total / N,
+            p = v.positivo / v.total,
+            n = v.negativo / v.total;
+        merito += r * infor(p, n);
+    });
+
+    data[key].merito = merito;
+}
+
+function infor(p, n){
+    return (p == 0 || n == 0) ? 0 : -p * Math.log2(p) -n * Math.log2(n);
+}
---
+++ practica3/bayes.py
@@ -0,0 +1,94 @@
+# ALGORITMO DE BAYES
+numAtributos = 4 # see: http://archive.ics.uci.edu/ml/datasets/Iris
+numClases = 2
+
+# Funciones
+def media(suma, N):
+    retSuma = [0.0] * len(suma)
+    it = 0
+
+    for col in suma:
+        retSuma[it] = col / N
+        it += 1
+
+    return retSuma
+
+    
+# Leer el fichero general
+IrisClases = open('Iris2Clases.txt', 'r')
+lines = IrisClases.readlines()
+
+# Preparamos el conjunto de datos
+data = []
+for line in lines:
+    data.append(line.replace('\n', '').split(','))
+
+# diccionario <clase, [a,b...]>
+dataset = dict()
+for i in range(len(data)):
+    vector = data[i]
+    className = vector[-1]
+    className = className.replace('\n', '')
+    if (className not in dataset):
+        dataset[className] = list()
+    dataset[className].append(vector)
+
+
+# print(dataset)
+
+sumas = [0.0] * 4 # siempre hay 4 valores -> almacenar en cada posicion la suma de cada columna
+
+# diccionario <clase, mediaClase>
+mediasClases = dict()
+for clase in dataset:
+    for fila in dataset[clase]:
+        for i in range(numAtributos):
+            sumas[i] += float(fila[i])
+    
+    # media de cada clase
+    mediasClase = media(sumas, len(dataset[clase]))
+    mediasClases[dataset[clase][0][-1]] = mediasClase
+    sumas = numAtributos*[0.0]
+
+
+files = [ 'TestIris01.txt', 'TestIris02.txt', 'TestIris03.txt' ]
+
+# print(mediasClases)
+# print("---------")
+
+for filename in files:
+    dictRestas = dict()
+
+    file = open(filename, 'r')
+    muestra = file.readline().replace('\n', '').split(',')
+
+    # START (1) recorrer ambas clases para sacar los valores de las medias
+    for nombreClase in mediasClases: 
+        if (nombreClase not in dictRestas):
+            dictRestas[nombreClase] = [0.0] * numAtributos
+        
+        # START (2) sumar todas las medias
+        for i in range(len(mediasClases[nombreClase])):
+            dictRestas[nombreClase][i] = float(muestra[i]) - mediasClases[nombreClase][i]
+        # END (2)
+    # END (1)
+    # print(dictRestas)
+
+    sumaTotal = 0.0
+    sumaMin = 999999.0
+    solClase = ''
+    # START (3) de la diferencia entre muestra/media de cada clase nos quedamos con la MENOR -> Majanouris
+    for nombreClase in dictRestas:
+        lista = dictRestas[nombreClase]
+        for it in range(len(lista)):
+            sumaTotal += pow(lista[it], 2)
+        
+        if(sumaTotal < sumaMin):
+            sumaMin = sumaTotal
+            solClase = nombreClase
+        
+        sumaTotal = 0.0
+    # END (3)
+
+    print("La solucion para " + filename + " es: " + solClase)
+
